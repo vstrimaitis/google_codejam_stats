7a3546ec89ac69f815af459af3b266248d1d1234
Vytautas Strimaitis <vstrimaitis@gmail.com>
2019-04-19 19:42:08 +0300

    Added version (date and time of last update to the site) to the header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -7,6 +7,17 @@ interface HeaderProps extends WithStyles<typeof mainStyles> {
     onDrawerToggle: () => void;
 }
 
+const formatLastUpdateDate = () => {
+    const d = new Date(document.lastModified);
+    return "v" +
+        d.getUTCFullYear().toString().padStart(4, "0") +
+        (d.getUTCMonth() + 1).toString().padStart(2, "0") +
+        d.getUTCDate().toString().padStart(2, "0") +
+        "." +
+        d.getUTCHours().toString().padStart(2, "0") +
+        d.getUTCMinutes().toString().padStart(2, "0");
+}
+
 export const Header = withStyles(mainStyles)(({ classes, onDrawerToggle }: HeaderProps) => (
     <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
@@ -20,6 +31,9 @@ export const Header = withStyles(mainStyles)(({ classes, onDrawerToggle }: Heade
             </IconButton>
             <Typography variant="h6" color="inherit" noWrap>
                 Google Code Jam Statistics
+                <Typography variant="caption" color="inherit" noWrap>
+                    {formatLastUpdateDate()}
+                </Typography>
             </Typography>
         </Toolbar>
     </AppBar>
