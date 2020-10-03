56df461173db1273a8b249b2de1f8b933fd99c09
Vytautas Strimaitis <vstrimaitis@gmail.com>
2020-10-03 18:33:45 +0300

    Sort problems of equal value by title

--- client/src/utils/task.ts
+++ client/src/utils/task.ts
@@ -4,4 +4,9 @@ export const totalValue = (task: Task): number =>
     task.tests.map(t => t.value).reduce((v, s) => v + s);
 
 export const sortByTotalValue = (tasks: Task[]): Task[] =>
-    tasks.sort((a, b) => totalValue(a) - totalValue(b));
+    tasks.sort((a, b) => {
+        const valA = totalValue(a);
+        const valB = totalValue(b);
+        if (valA === valB) return a.title.localeCompare(b.title);
+        return valA - valB;
+    });
