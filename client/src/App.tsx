34b564224f0513fb5e535103ef3a0b551fd63baa
Vytautas Strimaitis <vstrimaitis@gmail.com>
2020-10-03 21:10:54 +0300

    Add initial implementation of react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import { MainView } from "./views/MainView";
 import { createMuiTheme, MuiThemeProvider, CssBaseline } from "@material-ui/core";
 import { pink, indigo } from "@material-ui/core/colors";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+  } from "react-router-dom";
+
 
 const theme = createMuiTheme({
     palette: {
@@ -13,10 +19,14 @@ const theme = createMuiTheme({
 
 const App = () => (
     <MuiThemeProvider theme={theme}>
+        <Router>
         <React.Fragment>
             <CssBaseline />
-            <MainView />
+            <Switch>
+                <Route path="/:roundId?" children={<MainView />} />
+            </Switch>    
         </React.Fragment>
+        </Router>
     </MuiThemeProvider>
 );
 
