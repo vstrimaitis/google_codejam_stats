675ddb2196452c92bf7619ff951f35082e5d90a4
Vytautas Strimaitis <vstrimaitis@gmail.com>
2021-10-06 10:20:56 +0300

    Add 2021 Round 1B results (#43)
    * Fix conversion function not working with lists of strings which were introduced recently
    
    * Add results for 2021 R1B

--- convert.py
+++ convert.py
@@ -3,6 +3,8 @@ def to_camel_case(s):
     return parts[0] + "".join(x.title() for x in parts[1:])
 
 def json_to_camel_case(obj):
+    if not isinstance(obj, dict):
+        return obj
     new_obj = {}
     for k in obj:
         v = obj[k]
